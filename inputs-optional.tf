6bb117700599987235492b04caabfe924466a1f9
Rob Marks <robertm@kainos.com>
2022-05-09 11:51:17 +0100

    DTSPO-8176: IaaS Automation

---
+++ inputs-optional.tf
@@ -0,0 +1,23 @@
+variable "location" {
+  description = "Target Azure location to deploy the resource."
+  type        = string
+  default     = "UK South"
+}
+
+variable "common_tags" {
+  description = "Common tag to be applied to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "log_analytics_retention_period" {
+  description = "Log Analytics retention period in days."
+  type        = number
+  default     = 30
+}
+
+variable "log_analytics_sku" {
+  description = "Log Analytics instance SKU."
+  type        = string
+  default     = "PerGB2018"
+}
---
+++ inputs-required.tf
@@ -0,0 +1,17 @@
+variable "resource_group_name" {
+  description = "Enter Resource Group name."
+  type        = string
+  default     = null
+}
+
+variable "automation_account_name" {
+  description = "Enter automation account name."
+  type        = string
+  default     = null
+}
+
+variable "log_analytics_name" {
+  description = "Enter log analytics instance name."
+  type        = string
+  default     = null
+}
