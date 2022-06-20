ca36711c7a76863ed943842edddcbaf9816a21cd
mokainos <72137340+mokainos@users.noreply.github.com>
2022-06-20 11:53:25 +0100

    log_update_solution

--- linked-service.tf
+++ linked-service.tf
@@ -3,3 +3,20 @@ resource "azurerm_log_analytics_linked_service" "la_linked_service" {
   workspace_id        = azurerm_log_analytics_workspace.log_analytics_workspace.id
   read_access_id      = azurerm_automation_account.automation_account.id
 }
+
+
+resource "azurerm_log_analytics_solution" "update_solution" {
+  solution_name         = "Updates"
+  location              = var.location
+  resource_group_name   = var.resource_group
+  workspace_resource_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  workspace_name        = azurerm_log_analytics_workspace.log_analytics_workspace.name
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/Updates"
+  }
+  depends_on = [
+    azurerm_log_analytics_linked_service.la_linked_service
+  ]
+
+}
